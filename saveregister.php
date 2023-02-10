80ccaf781f2b28fd607931a1a337892674e8a99c
MyJinjiii <golfza123golfza123@gmail.com>
2023-02-10 19:19:46 +0700

    9

--- saveregister.php
+++ saveregister.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
     $name=@$_POST['name'];
     $sname=@$_POST['sname'];
@@ -20,27 +19,4 @@
         header("refresh:3; url=login.php");
     }
 //}
-=======
-<?php
-    $name=@$_POST['name'];
-    $sname=@$_POST['sname'];
-    $username=@$_POST['username'];
-    $password=@$_POST['password'];
-    $tel=@$_POST['tel'];
-    $adds=@$_POST['adds'];
-    
-    include("../web/connect.php");
-    print_r($_FILES);
-  //  if(!isset($_POST['filename'])){
-   /* $dir="upload/".$_FILES["filename"]["name"];
-    $filename=$_FILES["filename"]["name"];
-   move_uploaded_file($_FILES["filename"]["tmp_name"],$dir);*/
-   $save="insert into customer(id,name,sname,username,password,tel,adds) values('','$name','$sname','$username','$password','$tel','$adds')";
-    $con->query($save);
-    if($con==true){
-        echo"เพิ่มข้อมูลสำเร็จ";
-        header("refresh:3; url=login.php");
-    }
-//}
->>>>>>> 771019e2176f0f80330e826e39ec7c9dbb2fa6aa
     ?>
