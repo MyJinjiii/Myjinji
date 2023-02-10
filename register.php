771019e2176f0f80330e826e39ec7c9dbb2fa6aa
MyJinjiii <124889823+MyJinjiii@users.noreply.github.com>
2023-02-10 18:36:35 +0700

    Add files via upload

---
+++ register.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        td{
+            border:1px black solid,width:100px;
+            text-align:center;
+        
+        }
+        </style>
+</head>
+<body>
+<form action="saveregister.php" name="customer" method="post" enctype="multipart/form-data"> 
+    <table style="border-spacing:0px" align="center">
+    <td colspan="2" style="text-align:center;border:1px black solid;">
+    <h3>สมัครสมาชิก</h3></td>
+    <tr><td style="text-align:center;border:1px black solid;">ชื่อ:</td>
+    <td> <input type="text" name="name" placeholder="ชื่อ" required><br></td></tr>
+    <tr><td style="text-align:center;border:1px black solid;">สกุล:</td>
+    <td> <input type="text" name="sname" placeholder="สกุล" required><br></td></tr>
+    <tr><td style="text-align:center;border:1px black solid;">username:</td>
+    <td>  <input type="text" name="username" placeholder="username" required><br></td></tr>
+    <tr><td style="text-align:center;border:1px black solid;">password:</td>
+    <td><input type="password" name="password" placeholder="password" required><br></td></tr>
+    <tr><td style="text-align:center;border:1px black solid;">เบอร์โทร:</td>
+    <td><input type="tel" name="tel" placeholder="tel" pattern=[0-9]{3}[0-9]{7} required><br></td></tr>
+    <tr><td style="text-align:center;border:1px black solid;">ที่อยู่</td>
+    <td> <input type="text" name="adds" placeholder="ที่อยู่" required><br></td></tr>
+    
+   
+    <td colspan="2" style="width:200px;"><input type="submit" value="บันทึก"><input type="reset" value="ยกเลิก"></td>
+    </table>
+    </form>
+    
+  
+    
+
+
+    
+</body>
+</html>
